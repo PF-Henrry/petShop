92b4f08b018cb0ee793801d419c21004facff12d
ManuD33P <manu07x@hotmail.com>
2023-12-12 03:21:48 -0300

    iniciacion del repositorio

---
+++ src/libs/mongodb.js
@@ -0,0 +1,23 @@
+import {connect,connection} from "mongoose";
+
+
+const conn = {
+    isConnected:false
+}
+
+const {DB_URI} = process.env 
+export async function connectDB(){
+    if(conn.isConnected) return;
+        const db = await connect(DB_URI);
+        console.log(db.connection.db.databaseName);
+        conn.isConnected = db.connections[0].readyState;
+   
+    connection.on('connected',()=>{
+        console.log('Mongoose is connected');
+    });
+
+    connection.on('error',(err)=>{
+        console.log('Mongoose conection error', err);
+    });
+    
+}
