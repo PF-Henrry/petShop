6188383d7abcd977bc3793dc81d1caf1e20b4596
Evelyn-Nobile <nobileevelyn1@gmail.com>
2023-12-15 23:40:46 -0300

    Se elimino los componentes perfil y editar perfil para colocarlos en ramas separadas. Se agrego link al registro' git commit -m [200~Se elimino los componentes perfil y editar perfil para colocarlos en ramas separadas. Se agrego link al registro

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React from "react";
+import Link from 'next/link'
 import InputAdornment from '@mui/material/InputAdornment'
 import IconButton from '@mui/material/IconButton'
 import InputLabel from '@mui/material/InputLabel'
@@ -10,7 +11,7 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff'
 import { Button } from "@mui/material"
 import { useFormik } from "formik"
 import * as Yup from "yup"
-import { EMAIL_CHECKED, SIMPLE_PASSWORD_CHECKED } from '/src/utils/regex'
+import { EMAIL_CHECKED, PASSWORD_CHECKED } from '/src/utils/regex'
 
 
 
@@ -51,7 +52,7 @@ const Login = () => {
       password: Yup.string()
         .required("La contraseña es requerida")
         .matches(
-          SIMPLE_PASSWORD_CHECKED,
+          PASSWORD_CHECKED,
           "La contraseña debe contener al menos una letra, un número y tener una longitud mínima de 8 caracteres"
         ),
     }),
@@ -65,12 +66,12 @@ const Login = () => {
 
   return (
     <form onSubmit={handleSubmit} onKeyDown={handleKeyDown} >
-    <div class="flex flex-col items-center justify-center space-y-6 mt-14 ">
+    <div class="flex flex-col items-center justify-center space-y-6 mt-14 gap-1">
       <button class="flex items-center mb-2 justify-center transition ease-in-out delay-50 px-3 py-2.5 space-x-2
        bg-white border border-slate-600 rounded-md hover:bg-gray-100 
       focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600 focus:ring-opacity-50 ">
 
-                <svg viewBox="0 0 48 48" width="24" height="24" version="1.1" xmlns="http://www.w3.org/2000/svg" 
+                <svg viewBox="0 0 48 48" width="35" height="24" version="1.1" xmlns="http://www.w3.org/2000/svg" 
                 xmlnsXlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" 
                 stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Google-color</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Color-" transform="translate(-401.000000, -860.000000)"> <g id="Google" transform="translate(401.000000, 860.000000)"> <path d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24" id="Fill-1" fill="#FBBC05"> </path> <path d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333" id="Fill-2" fill="#EB4335"> </path> <path d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667" id="Fill-3" fill="#34A853"> </path> <path d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24" id="Fill-4" fill="#4285F4"> </path> </g> </g> </g> </g></svg>
                 <span class="text-gray-700 font-medium">Continuar con Google</span>
@@ -94,7 +95,7 @@ const Login = () => {
           
         /> */}
 
-<FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+<FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
           <InputLabel htmlFor="outlined-adornment">E-mail</InputLabel>
           <OutlinedInput
             id="outlined-adornment-email"           
@@ -110,7 +111,7 @@ const Login = () => {
         </FormControl>
 
 
-<FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+<FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
           <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
           <OutlinedInput
             id="outlined-adornment-password"
@@ -136,6 +137,10 @@ const Login = () => {
     <div style={{ color: 'red' }}>{errors.password}</div>
   )}
         </FormControl>
+        <div class="flex gap-4 font-semibold">
+        <p>¿No tienes cuenta?</p> <Link class="hover:text-customSecondary 
+        transition-colors duration-300"href="/signup">Regístrate aquí</Link>
+        </div>
 
         <Button
   type="submit"
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -7,8 +7,7 @@ export const URL_CHECKED = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\
 // debe cumplir con al menos 1 mayuscula, un numero, un caracter especial, no admite espacios. y tiene que tener un min de 8 y un maximo de 15
 export const PASSWORD_CHECKED = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$/
 
-//debe contener numeros y letras y un minimo de 8 caracteres
-export const SIMPLE_PASSWORD_CHECKED =  /^(?=.*[A-Za-z])(?=.*\d).{8,}$/
+
 
 // regex que valida los nombres, y apellidos pueden usar solo letras, espacios, letras con acentos, y pueden tener un min de 2 y
 // un maximo de 11 letras.
@@ -21,11 +20,7 @@ export const INPUT_NAME_CHECKED = /^(?=.{2,50}$)[a-zA-ZÀ-ÿ ]+$/;
 
 export const EMAIL_CHEKED = /^[a-zA-Z0-9._%+-]+@(?:hotmail|gmail|yahoo)\.(?:com|com\.mx|es|net)$/
 
-//verifica que el numero de tarjeta coincida con alguno de los patrones: mastercard, visa o amex
-export const CARD_CHECKED = /^(3[47][0-9]{13}|5[1-5][0-9]{14}|4[0-9]{12}(?:[0-9]{3})?)$/
 
-//verifico que el documento contenga solo números
-export const ID_CHECKED = /^[0-9]+$/
 
 //valida el codigo de area
 export const AREA_CODE_CHECKED = /^\d{2,4}$/
