343fe655ed1909a40b0e83dbd01b88ffcfa75476
ManuD33P <manu07x@hotmail.com>
2023-12-19 18:18:20 -0300

    middleware para rutas protegidas

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
-
-export const config = { matcher: ["/profile"] }
+//en matcher color las rutas que desean proteger.
+export const config = { matcher: ["/profile"] } 
