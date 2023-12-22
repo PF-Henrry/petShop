74d491a1d598b222900c2b45187c878e2022e256
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2023-12-22 16:58:46 -0300

    Update route.js
    pequeño error de sintaxis

--- src/app/api/products/filter/route.js
+++ src/app/api/products/filter/route.js
@@ -6,13 +6,12 @@ export async function POST(request){
    try {
       const data = await request.json();
       const {query} = data;
-      const SearchCriteria = {}
+      if(!query) throw TypeError('Query is undefined');
       for(const prop in query){
          if(query[prop]?._id){
             query[prop]._id = new mongoose.Types.ObjectId(query[prop]._id);
          }
       }
-      if(!query) throw TypeError('Query is undefined');
       const res = await products.find({...query});
       if(!res) throw TypeError('Product is not found')
       return NextResponse.json(res,{...query});
