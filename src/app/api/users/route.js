bc1b473d43ef2d8e0943d31365448d148b21ff58
Josue Riveros <josuesmitr@gmail.com>
2023-12-26 16:36:03 -0500

    Commit

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,18 +1,25 @@
 import { NextResponse } from "next/server";
-
+import { addUser } from "@/libs/createUserWithRelation";
 import { connectDB, conn } from "@/libs/mongodb";
 import userDB from '@/models/users';
 import { PASSWORD_CHECKED } from "@/utils/regex";
 
-//tarea: hacer GET y DELETE, y hacer funcionar esa wea, NO HACER POST AUN
-//GET por el momento que obtenga a todos normal
 
 export async function GET(request){  //         /api/users [GET]
 
 try {
 
 if(!conn.isConnected) connectDB();    
-const users = await userDB.find();
+const users = await userDB.find({}, {password: 0})
+// .populate("City", {
+// _id: 0,
+// name: 1
+// })
+// .populate("provinces", {
+// _id: 0,
+// name: 1
+// })
+
 
 return NextResponse.json(users, {
   status:200
@@ -25,4 +32,24 @@ return NextResponse.json(users, {
 }
 }
 
+
+export async function POST(request){
+  try {
+  
+    if(!conn.isConnected) connectDB()
+    const dataUser = await request.json()
+    const newUser = new addUser(dataUser)
+    // await newUser.save()
+    const findUser = await userDB.findOne({_id:newUser._id})
+
+    if(findUser) return NextResponse.json(findUser,{
+      status:200
+   })
+
+  } catch(error){
+    return NextResponse.json({mensaje: "Algo salió mal"}, {
+      status: 405
+  })
+  }
+}
 
