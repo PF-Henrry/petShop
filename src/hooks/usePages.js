7b7bc659a04af18cb2b0c8d206c55a42ac9e35fa
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2023-12-30 18:54:13 -0300

    filtrado

--- src/hooks/usePages.js
+++ src/hooks/usePages.js
@@ -1,10 +1,56 @@
 // En tu estado global (Zustand)
 import {create} from 'zustand';
-
+ 
 export const useProductStore = create((set,get) => ({
   products: [], // Aquí se almacenarían tus datos de productos
   sizeGroup: 9, // Tamaño de grupo por defecto
   currentPage: 1, // Página actual por defecto
+  filter:{},
+
+
+  setFilter : ({category,specie}) => set({filter: {category,specie}}),
+
+  getProductsFilter: async  () => {
+    try {
+
+      
+
+      const {filter,setProducts,getArrayPage} = get();
+      
+      let result = {};
+
+      if(!filter.category && !filter.specie) throw TypeError('no se establecieron filtros');
+      
+      if(filter.category) result = {...result,category: {...filter.category}}
+
+      if(filter.specie) result = {...result, specie: {...filter.specie}}
+
+      
+      if(!result) return false
+      const newProducts = await fetch('api/products/filter',{
+        method:'POST',
+        headers:{'Content-Type': 'application/json'},
+        body:{
+          query:{
+            ...result
+          }
+        }
+      });
+
+      const productPush = await newProducts.json();
+
+      if(productPush.ok){
+        setProducts(productPush);
+       return  getArrayPage();
+      }
+
+
+      
+    } catch (error) {
+        return error.message
+    }
+
+  },
 
   setProducts: (products) => set({ products }),
 
