ca513bfb2043499660db689f7a04b6a9f9bd041b
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2023-12-31 18:07:19 -0300

    fixruta

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -14,7 +14,7 @@ export async function GET(request,{params}) {
             _id:0,
             name:1
         })
-        .populate("specie",{
+        .populate("species",{
             _id:0,
             name:1,
         })
