f89fcf099d848051f95fbd23b01df612aee748cf
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2023-12-31 04:04:30 -0300

    filter provisional

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,58 @@
+import { useEffect, useState } from "react";
 import "./Filter.css";
+import { useProductStore } from "@/hooks/usePages";
 
-export default function Filter() {
+
+export default function Filter({handleOnChange,handleOnClick}) {
+    const {setProducts,setDataId,getDataId} = useProductStore();
+    const [inputs,setInputs] = useState();
+    const [data, setData] = useState();
+
+    // la primera vez que se renderiza el componente.
+  useEffect(()=>{
+
+        fetch('api/infoids')
+        .then(data => data.json())
+        .then(data => {
+          setData({category:data.category,species:data.specie});
+        })
+
+  },[]);
+
+
+
   return (
     <div className="filter-container flex flex-col bg-white">
       <h2 style={{ fontSize: "20px" }} className="p-2">
         Filtrar por:
       </h2>
       <div className="font-bold text-xl">Animal</div>
-      <select id="Specie" className="mb-2 p-3">
-        <option value={gatoId}>Gato</option>
-        <option value={perroId}>Perro</option>
-        <option value={otrosId}>Otros</option>
+      <select onChange={(e)=> handleOnChange(e)} name="species" className="mb-2 p-3">
+        {
+        data?.species.length && data.species.map((specie) => {
+
+          return(
+            <option 
+            key={specie?._id}
+            value={specie?._id}
+            >{specie?.name}</option>
+          )
+        }) 
+        }
       </select>
       <div className="font-bold text-xl">Categoría</div>
-      <select id="Category" className="mb-2 p-3">
-        <option value={accesorioCategoryId}>Accesorios</option>
-        <option value={alimentoCategoryId}>Alimento</option>
-        <option value={higieneSaludCategoryId}>Salud e Higiene</option>
+      <select onChange={(e) => handleOnChange(e) } name='category' className="mb-2 p-3">
+      {
+        data?.category.length && data.category.map((c) => {
+          
+          return(
+            <option 
+            key={c?._id}
+            value={c?._id}
+            >{c?.name}</option>
+          )
+        }) 
+        }  
       </select>
 
       <h2 style={{ fontSize: "20px" }} className="p-2">
@@ -31,7 +67,7 @@ export default function Filter() {
           <input type="checkbox" className="mr-2" /> Menor precio
         </label>
       </li>
-      <button onClick={handleChange}>Filtrar</button>
+      <button onClick={handleOnClick} >Filtrar</button>
     </div>
   );
 }
--- src/components/NavPages/NavPages.jsx
+++ src/components/NavPages/NavPages.jsx
@@ -1,28 +1,29 @@
 'use client'
 
-import { useProductStore } from "@/hooks/usePages";
+import { useProductStore,useCurrentPage } from "@/hooks/usePages";
 
 
 function NavPages(){
     const productsStore = useProductStore();
     const totalPages = productsStore.getTotalPages();
     const pages = Array.from({length:totalPages});
-    
+    const currentPage = useCurrentPage();    
 
 
 
     const handleChangePage = (page)=> {
-            productsStore.setCurrentPage(page);
+            productsStore.setCurrentPage(parseInt(page));
+
     }
 
     return (
         <div>
             {totalPages && pages.map((_,index)=> (
-           (<button key={index} name={index} onClick={(e) => handleChangePage(e.target.name)}>
+           (<button key={index} name={index+1} onClick={(e) => handleChangePage(e.target.name)}>
            {index+1}
           </button> ))
             )}
-
+            <h1>Actual: {currentPage}</h1>
             </div>
     )
 
