add21646331d78aef9fcd3aab2df7baf98aadb88
Juan5 <juangm2302@gmail.com>
2024-01-07 16:29:14 -0600

    StylesFavoritesFinished

--- src/app/favorites/page.jsx
+++ src/app/favorites/page.jsx
@@ -1,18 +1,30 @@
-'use client';
+"use client";
+
 import React, { useEffect, useState } from "react";
 import CardProduct from "@/components/CardsProducts/CardProduct";
 import { useProductStore } from "@/hooks/usePages";
+import "./Favorites.css";
+import {
+  ArrowRight,
+  CaretRight,
+  HeartStraight,
+  Storefront,
+  WarningCircle,
+} from "@phosphor-icons/react/dist/ssr";
+import { Breadcrumbs, Typography } from "@mui/material";
+import Link from "next/link";
+import Tippy from "@tippyjs/react";
 
 const FavoriteProducts = () => {
   const favorites = useProductStore((state) => state.getFavorites());
-  console.log('favorites', favorites)
+  // console.log("favorites", favorites);
 
   const [favoriteProducts, setFavoriteProducts] = useState([]);
   const fetchProductDetails = async (productId) => {
     try {
       const response = await fetch(`/api/products/${productId}`);
       if (!response.ok) {
-        throw new Error('Error al obtener detalles del producto');
+        throw new Error("Error al obtener detalles del producto");
       }
       const productDetails = await response.json();
       return productDetails;
@@ -24,7 +36,9 @@ const FavoriteProducts = () => {
 
   useEffect(() => {
     const fetchFavoriteProducts = async () => {
-      const detailsPromises = favorites.map((productId) => fetchProductDetails(productId));
+      const detailsPromises = favorites.map((productId) =>
+        fetchProductDetails(productId)
+      );
       const details = await Promise.all(detailsPromises);
       setFavoriteProducts(details.filter((product) => product !== null));
     };
@@ -32,35 +46,76 @@ const FavoriteProducts = () => {
     fetchFavoriteProducts();
   }, [favorites]);
 
-
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-gray-800 flex items-center justify-center mt-6">Tus Productos Favoritos</h1>
+    <div className="favorites-container">
+      <div className="nav-breadcrumbs">
+        <Breadcrumbs
+          separator={<CaretRight size={15} />}
+          aria-label="breadcrumb"
+        >
+          <Link underline="hover" color="inherit" href="/">
+            Inicio
+          </Link>
+          <Link underline="hover" color="inherit" href="/shop">
+            Tienda
+          </Link>
+          <Typography color="text.primary">Favoritos</Typography>
+        </Breadcrumbs>
+      </div>
+      <h1 className="favorites-title">
+        Mis Favoritos <HeartStraight size={32} />
+      </h1>
 
       {favoriteProducts?.length ? (
-      <div className="flex flex-wrap gap-10 justify-around items-center">
-        {favoriteProducts.map((product) => (
-          <CardProduct
-            key={product?._id}
-            id={product?._id}
-            name={product?.name}
-            price={product?.price}
-            detail={product?.detail}
-            image={product?.image}
-            brand={product?.brand?.name}  
-            specie={product?.species[0]?.name}  
-            category={product?.category[0]?.name}  
-           
-          />
-        ))}
-      </div>
-    ) : (
-      <p className="text-gray-600 flex items-center justify-center h-screen">No hay productos favoritos.</p>
-    )}
+        <div className="favorite-products-container">
+          <span className="favorite-products-info">
+            <p className="favorite-products-text">
+              Aquí puedes encontrar tus productos favoritos.
+            </p>
+            <p className="favorite-products-text-2">
+              {" "}
+              (Haz click sobre el icono de corazón para eliminar de favoritos.)
+            </p>
+          </span>
+          <div className="favorite-products">
+            {favoriteProducts.map((product) => (
+              <CardProduct
+                key={product?._id}
+                id={product?._id}
+                name={product?.name}
+                price={product?.price}
+                detail={product?.detail}
+                image={product?.image}
+                brand={product?.brand?.name}
+                specie={product?.species[0]?.name}
+                category={product?.category[0]?.name}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <span className="favorites-empty">
+          <p className="empty-title">
+            No hay productos favoritos{" "}
+            <Tippy content="Debe agregar productos a favoritos">
+              <WarningCircle size={32} />
+            </Tippy>
+          </p>
+          <p className="empty-text ">
+            Cada vez que agregues un producto a favoritos, aparecerán en esta
+            sección.
+          </p>
+          <Link href="/shop" className="go-to-shop">
+            <p>Ir a la tienda</p>
+            <span>
+              <ArrowRight size={32} />
+              <Storefront size={32} />
+            </span>
+          </Link>
+        </span>
+      )}
     </div>
   );
 };
 
 export default FavoriteProducts;
-
-
