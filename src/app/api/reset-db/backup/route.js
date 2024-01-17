c2dad483c0a387098dd1016900eb173d3992b098
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2024-01-17 01:31:47 -0300

    se agrego para solo administradores poder hacer peticiones a api/resetDB

--- src/app/api/reset-db/backup/route.js
+++ src/app/api/reset-db/backup/route.js
@@ -14,7 +14,7 @@ export async function GET(){
 
          const findUser = await Users.findOne({email:session.user.email});
 
-        if(findUser.role !== 1) throw TypeError('Unauthorized access');
+        if(findUser?.role !== 2) throw TypeError('Unauthorized access');
            const findAllProducts = await Products.find({ }).populate("category",{
             _id:0,
             name:1
